5562a9be2bf21659aa0c12585889ab11f91e85ee
Erick El nino <erick.tshimbombo@yahoo.com>
2023-05-16 00:21:51 +0200

    update data

--- Project 4/FormValidator-Start/script.js
+++ Project 4/FormValidator-Start/script.js
@@ -1,1 +1,17 @@
 // Code Starts Here..
+const form = document.querySelector("#form")
+
+console.log(form); 
+
+
+function validateForm(){
+    
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const username = document.querySelector("");
+    console.log(`Hello world`);
+    console.log(e.target);
+});
