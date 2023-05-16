c10cf292aefb97e293f3b12b84495737e0ad139e
Erick El nino <erick.tshimbombo@yahoo.com>
2023-05-16 02:55:10 +0200

    update on the form validation

--- Project 4/FormValidator-Start/script.js
+++ Project 4/FormValidator-Start/script.js
@@ -1,17 +1,29 @@
 // Code Starts Here..
 const form = document.querySelector("#form")
+const message = document.querySelector(".message");
+
 
 console.log(form); 
 
+let isValid = false;
 
 function validateForm(){
-    
+    isValid = form.checkValidity();
+
+   if(!isValid)
+   {
+        message.textContent = "Please fill out the form ";
+        message.style.color = "red";
+        
+   }
+
+   console.log(isValid);
 }
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
 
-    const username = document.querySelector("");
     console.log(`Hello world`);
     console.log(e.target);
+    validateForm();
 });
